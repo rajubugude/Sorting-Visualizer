fa794abd11364eeb9d72e63a3012cb12900064f4
Raju Bugude <88735632+rajubugude@users.noreply.github.com>
2022-09-01 10:44:18 +0530

    Update selectionsort.js

--- scripts/selectionsort.js
+++ scripts/selectionsort.js
@@ -4,25 +4,24 @@ async function selectionSort(){
       const n = divBars.length;
       for (let i = 0; i < n; i++){
             let minIdx = i;
-            divBars[minIdx].style.background="#578F06 "
+            divBars[minIdx].style.background="#578F06"
             for (let j = i+1; j < n; j++)
             {
-                  divBars[j].style.background="#7EAB3C";
-                  await delay(10)
+                  divBars[j].style.background="#59ad47";
+                  await delay()
                   if (parseInt(divBars[j].style.height) < parseInt(divBars[minIdx].style.height)){
-                        // divBars[minIdx].style.background = 'white';
                         minIdx = j;
-                        divBars[minIdx].style.background = '#578F06 ';
+                        divBars[minIdx].style.background = '#578F06';
                   }
                   if (minIdx != j){
                     divBars[j].style.background = 'white';
                   }
 
             }
-            await delay(10)
+            await delay()
             swap(divBars[minIdx], divBars[i]);
             divBars[minIdx].style.background = 'white';
-            divBars[i].style.background="#578F06 ";
+            divBars[i].style.background="#578F06";
             
       }
       enableSortingButton()
