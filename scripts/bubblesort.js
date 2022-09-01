e3adc698c768e0a2d6a80bbc7d203063a990bead
Raju Bugude <88735632+rajubugude@users.noreply.github.com>
2022-09-01 10:45:21 +0530

    Update bubblesort.js

--- scripts/bubblesort.js
+++ scripts/bubblesort.js
@@ -6,9 +6,9 @@ async function bubbleSort(){
       {
             for (let j = 0; j < n-i-1; j++)
             {
-                  divBars[j].style.background="#7EAB3C";
-                  divBars[j+1].style.background="#7EAB3C";
-                  await delay(100)
+                  divBars[j].style.background="#59ad47";
+                  divBars[j+1].style.background="#59ad47";
+                  await delay(10)
                   if (parseInt(divBars[j].style.height) > parseInt(divBars[j+1].style.height)){
                         swap(divBars[j],divBars[j+1]);
                   }
@@ -16,8 +16,8 @@ async function bubbleSort(){
                   divBars[j].style.background= "white";
                   divBars[j+1].style.background="white";
             }
-            // await delay(10)
-            divBars[n-1-i].style.background="#578F06";
+
+            divBars[n-1-i].style.background="#84ed13";
       }
       enableSortingButton();
 }
